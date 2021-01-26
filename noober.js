cb0a7ac189e4e419c428815c66869eb68e21ca1e
Tanvi Narain <tanvi.narain@kellogg.northwestern.edu>
2021-01-25 22:41:01 -0600

    commit

--- noober.js
+++ noober.js
@@ -8,6 +8,73 @@ function pageLoad() {
   // e.g. ask 'ride' for its length or only grab the first member of the Array
   console.log(ride)
 
+  levelofService = ['Noober X', 'Noober Purple', 'Nobber Pool']
+  if (ride.length > 1 || ride[0].numberOfPassengers == 1){
+    levelofService = 'Noober Pool'
+  } else if (ride.purpleRequested = true){
+    levelofService = 'Noober Purple'
+  } else if (ride.numberofPassengers > 3){
+    levelofService = 'Noober XL'
+  } else {levelofSerice = 'Noober X'}
+
+  console.log(levelofService)
+  let passenger1 = ride[0]
+  let passenger2 = ride[1]
+  let passenger3 = ride[2]
+  
+  if (ride.length == 1) {
+    passenger1Name = passenger1.passengerDetails.first + ' ' + passenger1.passengerDetails.last
+    passenger1Phone = passenger1.passengerDetails.phoneNumber
+    passenger1NumberofPassengers = passenger1.numberOfPassengers
+    passenger1PickupAddressLine1 = passenger.picklocation.address
+    passenger1PickupAddressLine2 = passenger1.pickupLocation.city + ', ' + passenger1.pickupLocation.state + ' ' + passenger1.pickupLocation.zip
+    passenger1DropoffAddressLine1 = passenger1.dropoffLocation.address
+    passenger1DropoffAddressLine2 = passenger1.dropoffLocation.city + ', ' + passenger1.dropoffLocation.state + ' ' + passenger1.dropoffLocation.zip
+  } else if (ride.length == 2) {
+    passenger1Name = passenger1.passengerDetails.first + ' ' + passenger1.passengerDetails.last
+    passenger1Phone = passenger1.passengerDetails.phoneNumber
+    passenger1NumberofPassengers = passenger1.numberOfPassengers
+    passenger1PickupAddressLine1 = passenger.picklocation.address
+    passenger1PickupAddressLine2 = passenger1.pickupLocation.city + ', ' + passenger1.pickupLocation.state + ' ' + passenger1.pickupLocation.zip
+    passenger1DropoffAddressLine1 = passenger1.dropoffLocation.address
+    passenger1DropoffAddressLine2 = passenger1.dropoffLocation.city + ', ' + passenger1.dropoffLocation.state + ' ' + passenger1.dropoffLocation.zip
+    passenger2Name = passenger2.passengerDetails.first + ' ' + passenger2.passengerDetails.last
+    passenger2Phone = passenger2.passengerDetails.phoneNumber
+    passenger2NumberOfPassengers = passenger2.numberOfPassengers
+    passenger2PickupAddressLine1 = passenger2.pickupLocation.address
+    passenger2PickupAddressLine2 = passenger2.dropoffLocation.address
+    passenger2DropoffAddressLine1 = passenger2.dropoffLocation.address
+    passenger2DropoffAddressLine2 = passenger2.dropoffLocation.city + ', ' + passenger2.dropoffLocation.state + ' ' + passenger2.dropoffLocation.zip
+  } else if (ride.length == 3) {
+    passenger1Name = passenger1.passengerDetails.first + ' ' + passenger1.passengerDetails.last
+    passenger1Phone = passenger1.passengerDetails.phoneNumber
+    passenger1NumberofPassengers = passenger1.numberOfPassengers
+    passenger1PickupAddressLine1 = passenger.picklocation.address
+    passenger1PickupAddressLine2 = passenger1.pickupLocation.city + ', ' + passenger1.pickupLocation.state + ' ' + passenger1.pickupLocation.zip
+    passenger1DropoffAddressLine1 = passenger1.dropoffLocation.address
+    passenger1DropoffAddressLine2 = passenger1.dropoffLocation.city + ', ' + passenger1.dropoffLocation.state + ' ' + passenger1.dropoffLocation.zip
+    passenger2Name = passenger2.passengerDetails.first + ' ' + passenger2.passengerDetails.last
+    passenger2Phone = passenger2.passengerDetails.phoneNumber
+    passenger2NumberOfPassengers = passenger2.numberOfPassengers
+    passenger2PickupAddressLine1 = passenger2.pickupLocation.address
+    passenger2PickupAddressLine2 = passenger2.dropoffLocation.address
+    passenger2DropoffAddressLine1 = passenger2.dropoffLocation.address
+    passenger2DropoffAddressLine2 = passenger2.dropoffLocation.city + ', ' + passenger2.dropoffLocation.state + ' ' + passenger2.dropoffLocation.zip
+    passenger3Name = passenger3.passengerDetails.first + ' ' + passenger3.passengerDetails.last
+    passenger3Phone = passenger3.passengerDetails.phoneNumber
+    passenger3NumberofPassengers = passenger3.numberOfPassengers
+    passenger3PickupAddressLine1 = passenger3.pickLocation.address
+    passenger3PickupAddressLine2 = passenger3.pickLocation.city + ', ' + passenger3.pickupLocation.state + ' ' + passenger3pickupLocation.zip
+    passenger3DropoffAddressLine1 = passenger3.dropoffLocation.address
+    passenger3DropoffAddressLine2 = passenger3.dropoffLocation.city + ', ' + passenger3.dropoffLocation.state + ' ' + passenger3.dropoffLocation.zip
+  }
+  console.log(passenger1PickUpAddressLine1)
+
+
+ 
+
+
+
   // these variables map to the elements on the finished page;
   // assign the proper value to each variable
 
